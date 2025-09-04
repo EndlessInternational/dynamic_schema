d9e8e3ee6d2437507ed3748772d82659ca76f7a6
Kristoph Cichocki <github.com@kristoph.net>
2025-09-04 08:52:14 -0700

    - implemented to_h for Struct

--- lib/dynamic_schema/struct.rb
+++ lib/dynamic_schema/struct.rb
@@ -32,27 +32,32 @@ module DynamicSchema
           end
 
           def initialize( attributes = nil )
-            @attributes = attributes ? attributes.dup : {}
+            @attributes = attributes&.dup
           end
 
           def to_h
-            @attributes.dup
+            result = {}
+            compiled_schema.each do | key, criteria |
+              property_name = ( criteria[ :as ] || key ).to_sym
+              value = __object_to_h( self.send( property_name ) ) 
+              result[ property_name ] = value if value
+            end
+            result
           end
 
           def validate!
-           ::DynamicSchema::Validator.validate!( to_h, schema: compiled_schema )
+           ::DynamicSchema::Validator.validate!( @attributes, schema: compiled_schema )
             self
           end
 
           def validate
-           ::DynamicSchema::Validator.validate( to_h, schema: compiled_schema )
+           ::DynamicSchema::Validator.validate( @attributes, schema: compiled_schema )
           end
 
           def valid?
-           ::DynamicSchema::Validator.valid?( to_h, schema: compiled_schema )
+           ::DynamicSchema::Validator.valid?( @attributes, schema: compiled_schema )
           end
 
-          # Define readers/writers from the compiled schema
           __compiled_schema.each do | key, criteria |
             property_name = ( criteria[ :as ] || key ).to_sym
             type = criteria[ :type ]
@@ -71,29 +76,50 @@ module DynamicSchema
             elsif type
               define_method( property_name ) do 
                 value = @attributes[ property_name ]
-                if criteria[ :array ]
-                  Array( value ).map do | v |
-                    ::DynamicSchema::Converter.convert( v, to: type ) do | v | 
-                      type.new( v ) || v rescue v
-                    end
-                  end
-                else 
-                  ::DynamicSchema::Converter.convert( value, to: type ) do | v | 
-                    type.new( v ) || v rescue v
-                  end
-                end
+                criteria[ :array ] ? 
+                  Array( value ).map { | v | __coerce( v, to: type ) } : 
+                  __coerce( value, to: type )
               end
             else
               define_method( property_name ) { @attributes[ property_name ] } 
             end
 
-            define_method( :"#{ property_name }=" ) { | value | @attributes[ property_name ] = value }
+            define_method( :"#{ property_name }=" ) do | value | 
+              @attributes[ property_name ] = value 
+            end
           end
 
         private
 
           def compiled_schema
             self.class.compiled_schema
+          end
+
+          def __object_to_h( object )
+            case object
+            when nil
+              nil
+            when ::Hash
+              object.transform_values { | v | __object_to_h( v ) } unless object.empty?
+            when ::Array
+              object.map { | e | __object_to_h( e ) } unless object.empty?
+            else
+              if object.respond_to?( :to_h )
+                __object_to_h( object.to_h )
+              else
+                object
+              end
+            end
+          end
+
+          def __coerce( value, to: )
+            if value.nil? || value.is_a?( to ) 
+              value
+            else
+              ::DynamicSchema::Converter.convert( value, to: to ) do | v | 
+                to.new( v ) || v rescue v 
+              end
+            end 
           end
 
         end
